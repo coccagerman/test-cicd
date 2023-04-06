0929aa674c1b812dd556eee961f6068e04adb190
Germán Cocca <coccagerman@gmail.com>
2023-04-06 11:43:01 -0300

    small change in prod

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 					count is {count}
 				</button>
 				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
+					Small change in prod
 				</p>
 			</div>
 			<p className='read-the-docs'>
